e3a50d0617828a84fd7f8f4506f7bf6a36fb5b45
Erick Chandra <erickchandra718@gmail.com>
2021-07-11 03:00:44 +0700

    Some Update

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -5,14 +5,51 @@
 		<header>
 			<div class="container">
 				<div class="row">
-					<div class="hide-text col-lg-4 pt-5">
+					<div class="hide-content col-lg-4 pt-5">
 						<h4 class="header-title">SMK Kristen Immanuel</h4>
 
 						<button type="button" class="button-primary">Explore More</button>
 					</div>
 
-					<div class="col-lg-8">
-						<div class="rect"></div>
+					<div class="hide-content col-lg-8 pt-3 pl-5 pt-5 pb-5">
+						<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+							<ol class="carousel-indicators">
+								<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+								<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+								<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+							</ol>
+							<div class="carousel-inner">
+								<div class="carousel-item active">
+									<img class="carousel-image" src="images/gedung.jpg" alt="First slide">
+									<div class="carousel-caption d-none d-md-block">
+										<h5>Lorem 1</h5>
+										<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+									</div>
+								</div>
+								<div class="carousel-item">
+									<img class="carousel-image" src="images/banner1.jpg" alt="Second slide">
+									<div class="carousel-caption d-none d-md-block">
+										<h5>Lorem 2</h5>
+										<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+									</div>
+								</div>
+								<div class="carousel-item">
+									<img class="carousel-image" src="images/coworking.jpeg" alt="Third slide">
+									<div class="carousel-caption d-none d-md-block">
+										<h5>Lorem 3</h5>
+										<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+									</div>
+								</div>
+							</div>
+							<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+								<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+								<span class="sr-only">Previous</span>
+							</a>
+							<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+								<span class="carousel-control-next-icon" aria-hidden="true"></span>
+								<span class="sr-only">Next</span>
+							</a>
+						</div>
 					</div>
 				</div>
 			</div>
