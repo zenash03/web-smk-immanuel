a4a7602a0436e4d888bfeb815f7153ba56cb700e
Erick Chandra <erickchandra718@gmail.com>
2021-07-11 14:35:21 +0700

    Update

--- vue/src/components/Nav.vue
+++ vue/src/components/Nav.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" class="wave"><path fill="#0099ff" fill-opacity="1" d="M0,0L18.5,21.3C36.9,43,74,85,111,117.3C147.7,149,185,171,222,170.7C258.5,171,295,149,332,122.7C369.2,96,406,64,443,69.3C480,75,517,117,554,122.7C590.8,128,628,96,665,101.3C701.5,107,738,149,775,170.7C812.3,192,849,192,886,192C923.1,192,960,192,997,213.3C1033.8,235,1071,277,1108,277.3C1144.6,277,1182,235,1218,218.7C1255.4,203,1292,213,1329,208C1366.2,203,1403,181,1422,170.7L1440,160L1440,0L1421.5,0C1403.1,0,1366,0,1329,0C1292.3,0,1255,0,1218,0C1181.5,0,1145,0,1108,0C1070.8,0,1034,0,997,0C960,0,923,0,886,0C849.2,0,812,0,775,0C738.5,0,702,0,665,0C627.7,0,591,0,554,0C516.9,0,480,0,443,0C406.2,0,369,0,332,0C295.4,0,258,0,222,0C184.6,0,148,0,111,0C73.8,0,37,0,18,0L0,0Z"></path></svg>
         <nav>
             <div class="container d-flex justify-content-between">
                 <div class="logo">
@@ -8,6 +9,8 @@
 
                 <div class="nav-right">
                     <ul class="nav-links">
+                        <li><a href="">Berita</a></li>
+                        <li><a href="">Aktivitas</a></li>
                         <li><a href="">Prestasi</a></li>
                         <li><a href="">Jurusan</a></li>
                         <li><a href="">Visi</a></li>
@@ -33,9 +36,16 @@
         </nav>
     </div>
 </template>
+
 <style>
+svg {
+    position: absolute;
+    z-index: -100;
+    transition: 1s;
+}
+
 nav {
-    margin-top: 2rem;
+    padding-top: 2rem;
 }
 
 nav .logo {
@@ -81,11 +91,16 @@ nav.nav {
 }
 
 .nav-links li button {
-    border: 2px solid gray;
+    border: none;
     padding: 5px 10px;
     background-color: white;
     font-size: 15px;
     font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    transition: .1s;
+}
+
+.nav-links li button:hover {
+    transform: translateY(-2px);
 }
 
 .social {
@@ -102,7 +117,7 @@ nav.nav {
     width: 30px;
     height: 30px;
     border-radius: 50%;
-    background-color: #ececec;
+    background-color: white;
     text-decoration: none;
     cursor: pointer;
     transition: .2s ease;
--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -3,7 +3,7 @@
 		<Nav></Nav>
 
 		<div id="transition"></div>
-		<header>
+		<header class="mb-5">
 			<div class="container">
 				<div class="row">
 					<div class="hide-content position-relative col-lg-4 pt-5 pb-5">
@@ -23,6 +23,250 @@
 				</div>
 			</div>
 		</header>
+
+		<main class="pt-5">
+			<section class="headline">
+				<div class="container">
+					<div class="row">
+						<div class="hide-content col-lg-4">
+							<h2 class="headline-title fade-out fade">Pendafaran SMK Immanuel Dibuka!</h2>
+							<p class="fade-out fade mb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, neque in officiis aliquam recusandae ipsam eaque autem quidem dicta enim tempora maxime excepturi totam mollitia nostrum rem necessitatibus, quam ...</p>
+
+							<button type="button" class="button-primary">Explore More</button>
+						</div>
+
+						<div class="position-relative col-lg-8">
+							<div class="image-main fade-out fade">
+								<img src="images/dummy/headline.png" alt="">
+							</div>
+
+							<div class="image-second fade-out fade">
+								<img src="images/gedung.jpg" alt="">
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<section class="news pt-5 mb-5">
+				<div class="container">
+
+					<div class="row">
+						<div class="col-lg-8">
+						<h2 class="title pop-out pop-up">Berita Terbaru</h2>
+							<div class="row">
+								<div class="col-lg-6 mb-2">
+									<div class="news-card">
+										<div class="img">
+											<img src="images/slideshow/1.jpg" alt="">	
+										</div>	
+
+										<div class="content">
+											<div class="tag">Lomba</div>
+
+											<h5 class="title"><a href="">Lorem ipsum dolor sit amet consectetur adipisicing elit</a></h5>
+
+											<div class="details">
+												<div class="author">
+													<i class="far fa-user"></i>
+													<p>Admin</p>
+												</div>
+
+												<div class="date">
+													<i class="far fa-clock"></i>
+													<p>July 9, 2021</p>
+												</div>
+											</div>
+										</div>
+									</div>	
+								</div>
+
+								<div class="col-lg-6 mb-2">
+									<div class="news-card">
+										<div class="img">
+											<img src="images/slideshow/1.jpg" alt="">	
+										</div>	
+
+										<div class="content">
+											<div class="tag">Lomba</div>
+
+											<h5 class="title"><a href="">Lorem ipsum dolor sit amet consectetur adipisicing elit</a></h5>
+
+											<div class="details">
+												<div class="author">
+													<i class="far fa-user"></i>
+													<p>Admin</p>
+												</div>
+
+												<div class="date">
+													<i class="far fa-clock"></i>
+													<p>July 9, 2021</p>
+												</div>
+											</div>
+										</div>
+									</div>	
+								</div>
+
+								<div class="col-lg-6 mb-2">
+									<div class="news-card">
+										<div class="img">
+											<img src="images/slideshow/1.jpg" alt="">	
+										</div>	
+
+										<div class="content">
+											<div class="tag">Lomba</div>
+
+											<h5 class="title"><a href="">Lorem ipsum dolor sit amet consectetur adipisicing elit</a></h5>
+
+											<div class="details">
+												<div class="author">
+													<i class="far fa-user"></i>
+													<p>Admin</p>
+												</div>
+
+												<div class="date">
+													<i class="far fa-clock"></i>
+													<p>July 9, 2021</p>
+												</div>
+											</div>
+										</div>
+									</div>	
+								</div>
+
+								<div class="col-lg-6 mb-2">
+									<div class="news-card">
+										<div class="img">
+											<img src="images/slideshow/1.jpg" alt="">	
+										</div>	
+
+										<div class="content">
+											<div class="tag">Lomba</div>
+
+											<h5 class="title"><a href="">Lorem ipsum dolor sit amet consectetur adipisicing elit</a></h5>
+
+											<div class="details">
+												<div class="author">
+													<i class="far fa-user"></i>
+													<p>Admin</p>
+												</div>
+
+												<div class="date">
+													<i class="far fa-clock"></i>
+													<p>July 9, 2021</p>
+												</div>
+											</div>
+										</div>
+									</div>	
+								</div>
+							</div>
+						</div>
+
+						<div class="col-lg-4">
+							<h2 class="title pop-out pop-up text-center">More</h2>
+
+							<div class="container">
+								<div class="news-list mb-2">
+									<i class="fas fa-hashtag"></i>
+									<p>Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
+								</div>
+
+								<div class="news-list mb-2">
+									<i class="fas fa-hashtag"></i>
+									<p>Lorem ipsum dolor.</p>
+								</div>
+
+								<div class="news-list mb-2">
+									<i class="fas fa-hashtag"></i>
+									<p>Lorem ipsum dolor.</p>
+								</div>
+
+								<div class="news-list mb-2">
+									<i class="fas fa-hashtag"></i>
+									<p>Lorem ipsum dolor.</p>
+								</div>
+
+								<div class="news-list mb-2">
+									<i class="fas fa-hashtag"></i>
+									<p>Lorem ipsum dolor.</p>
+								</div>
+
+								<div class="news-list mb-2">
+									<i class="fas fa-hashtag"></i>
+									<p>Lorem ipsum dolor.</p>
+								</div>
+
+								<div class="news-list mb-2">
+									<i class="fas fa-hashtag"></i>
+									<p>Lorem ipsum dolor.</p>
+								</div>
+
+								<div class="news-list mb-2">
+									<i class="fas fa-hashtag"></i>
+									<p>Lorem ipsum dolor.</p>
+								</div>
+
+								<div class="news-list mb-2">
+									<i class="fas fa-hashtag"></i>
+									<p>Lorem ipsum dolor.</p>
+								</div>
+
+								<div class="news-list mb-2">
+									<i class="fas fa-hashtag"></i>
+									<p>Lorem ipsum dolor.</p>
+								</div>
+
+								<div class="news-list mb-2">
+									<i class="fas fa-hashtag"></i>
+									<p>Lorem ipsum dolor.</p>
+								</div>
+
+								<div class="news-list mb-2">
+									<i class="fas fa-hashtag"></i>
+									<p>Lorem ipsum dolor.</p>
+								</div>
+
+								<div class="news-list mb-2">
+									<i class="fas fa-hashtag"></i>
+									<p>Lorem ipsum dolor.</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<section class="jurusan pt-5">
+				<div class="container d-flex">
+					<div class="content">
+						<h2 class="mb-5"><span class="line"></span> Jurusan <span class="line"></span></h2>
+
+						<div class="majors">
+							<div class="row">
+								<div class="col-lg-4">
+									<img src="images/coworking.jpeg" class="img-jurusan">
+									<h5>Akutansi dan Lembaga</h5>
+									<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit alias eaque ratione voluptatum quis libero fuga mollitia totam deleniti sint dolorem modi aliquid rem, accusamus fugit eius nesciunt vero quibusdam.</p>
+								</div>
+
+								<div class="col-lg-4">
+									<img src="images/coworking.jpeg" class="img-jurusan">
+									<h5>Teknik Komputer dan Jaringan</h5>
+									<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit alias eaque ratione voluptatum quis libero fuga mollitia totam deleniti sint dolorem modi aliquid rem, accusamus fugit eius nesciunt vero quibusdam.</p>
+								</div>
+								
+								<div class="col-lg-4">
+									<img src="images/coworking.jpeg" class="img-jurusan">
+									<h5>Bisnis dan Pemasaran</h5>
+									<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit alias eaque ratione voluptatum quis libero fuga mollitia totam deleniti sint dolorem modi aliquid rem, accusamus fugit eius nesciunt vero quibusdam.</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<section style="height: 1000px"></section>
+		</main>
 	</div>
 </template>
 
@@ -39,7 +283,9 @@ export default {
 		removeTransition() {
 			const transition = document.getElementById('transition');
 
-			transition.classList.add('out');
+			setTimeout(() => {
+				transition.classList.add('out');
+			}, 50);
 		}
 	},
 	mounted() {
