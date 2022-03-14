eca0114d1dfddeefb5b46499d2292686518a8db6
Jeki Gates <jekigates2004@gmail.com>
2022-03-14 22:46:34 +0700

    modify vue router and add custom width

---
+++ frontend-vue/src/views/EventView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="bg-light-100">
+    <div class="w-full min-h-screen text-dark-100 flex">
+      <aside class="w-72 border-r border-dark-25 bg-white">
+        <a href="" class="h-24 px-4 flex items-center justify-between border-b border-dark-25">
+          <img src="../assets/img/logo.png" alt="" class="w-14" />
+          <div>
+            <p class="text-h7 font-h1">SMK Immanuel</p>
+            <p class="text-caption">Smart, Wise, Accountable</p>
+          </div>
+        </a>
+      </aside>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EventView",
+};
+</script>
