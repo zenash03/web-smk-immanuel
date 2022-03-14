0dc9a45ac4c3fe7474e2682e353a5c9766e09e3e
Jeki Gates <jekigates2004@gmail.com>
2022-03-14 22:06:39 +0700

    customize font size and weight

--- frontend-vue/src/views/HomeView.vue
+++ frontend-vue/src/views/HomeView.vue
@@ -4,10 +4,10 @@
       <aside class="py-6 px-10 w-64 border-r border-gray-200 bg-light-blue">
         <a href="" class="flex items-center justify-center">
           <img src="" alt="" />
-          <button class="bg-primary-light-blue">Hey</button>
-          <button class="bg-primary-dark-blue">Hey</button>
-          <button class="bg-state-red">Hey</button>
         </a>
+        <h1 class="text-h1 font-h1">h1</h1>
+        <h1 class="text-h2 font-h2">h2</h1>
+        <h1 class="text-subline font-subline">subline</h1>
       </aside>
     </div>
   </div>
