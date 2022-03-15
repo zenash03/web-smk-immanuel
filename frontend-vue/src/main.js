8868e7183e14c7e0df7c39e4a34e7cc43f5c2447
Jeki Gates <jekigates2004@gmail.com>
2022-03-15 09:11:05 +0700

    install fontawesome

--- frontend-vue/src/main.js
+++ frontend-vue/src/main.js
@@ -4,12 +4,27 @@ import router from "./router";
 import store from "./store";
 import "./assets/css/tailwind.css";
 
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+/* import specific icons */
+import { faClipboard } from "@fortawesome/free-solid-svg-icons";
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/* add icons to the library */
+library.add(faClipboard);
+
+/* add font awesome icon component */
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: function (h) {
-    return h(App);
-  },
+    router,
+    store,
+    render: function (h) {
+        return h(App);
+    },
 }).$mount("#app");
--- frontend-vue/src/views/EventView.vue
+++ frontend-vue/src/views/EventView.vue
@@ -1,37 +1,62 @@
 <template>
-  <div class="bg-light-75">
-    <div class="w-full min-h-screen text-dark-100 flex">
-      <aside class="w-72 border-r border-dark-25 bg-white">
-        <a href="" class="h-24 px-4 flex items-center justify-between border-b border-dark-25">
-          <img src="../assets/img/logo.png" alt="" class="w-14" />
-          <div>
-            <p class="text-h7 font-h1">SMK Immanuel</p>
-            <p class="text-caption">Smart, Wise, Accountable</p>
-          </div>
-        </a>
-      </aside>
+    <div class="bg-light-75">
+        <div class="w-full min-h-screen text-dark-100 flex">
+            <aside class="w-72 border-r border-dark-25 bg-white">
+                <a href="" class="h-24 px-4 flex items-center justify-between border-b border-dark-25">
+                    <img src="../assets/img/logo.png" alt="" class="w-14" />
+                    <div>
+                        <p class="text-h7 font-h1">SMK Immanuel</p>
+                        <p class="text-caption">Smart, Wise, Accountable</p>
+                    </div>
+                </a>
 
-      <main class="flex-1 flex flex-col">
-        <div class="h-24 px-16 flex items-center justify-end text-dark-50 bg-white">
-          <p class="text-h7 font-h7">Hai, <span class="font-bold">Jeki Gates</span></p>
-        </div>
+                <nav class="text-primary-red">
+                    <ul>
+                        <li class="mt-1">
+                            <a href="">
+                                <span><font-awesome-icon icon="fa-solid fa-clipboard" /></span>
+                                <span>Event</span>
+                            </a>
+                            <ul>
+                                <li class="mt-1">
+                                    <a href=""
+                                        ><span><font-awesome-icon icon="fa-solid fa-clipboard" /></span>
+                                        <span>List Event</span>
+                                    </a>
+                                </li>
+                                <li class="mt-1">
+                                    <a href=""
+                                        ><span><font-awesome-icon icon="fa-solid fa-clipboard" /></span>
+                                        <span>Buat Event</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </nav>
+            </aside>
 
-        <div class="px-16 py-16">
-          <div class="px-16 py-16 text-dark-50 bg-white rounded-lg">
-            <p class="text-h7 font-h7">Welcome, <span class="font-bold">Jeki Gates</span></p>
-          </div>
-        </div>
+            <main class="flex-1 flex flex-col">
+                <div class="h-24 px-16 flex items-center justify-end text-dark-50 bg-white">
+                    <p class="text-h7 font-h7">Hai, <span class="font-bold">Jeki Gates</span></p>
+                </div>
 
-        <div class="h-24 px-16 flex items-center justify-end text-dark-50 bg-white mt-auto">
-          <p class="text-body font-body"><span class="font-bold">2021-2022</span> SMK IMMANUEL | Made with by IT CLUB</p>
+                <div class="px-16 py-16">
+                    <div class="px-16 py-16 text-dark-50 bg-white rounded-lg">
+                        <p class="text-h7 font-h7">Welcome, <span class="font-bold">Jeki Gates</span></p>
+                    </div>
+                </div>
+
+                <div class="h-24 px-16 flex items-center justify-end text-dark-50 bg-white mt-auto">
+                    <p class="text-body font-body"><span class="font-bold">2021-2022</span> SMK IMMANUEL | Made with by IT CLUB</p>
+                </div>
+            </main>
         </div>
-      </main>
     </div>
-  </div>
 </template>
 
 <script>
 export default {
-  name: "EventView",
+    name: "EventView",
 };
 </script>
